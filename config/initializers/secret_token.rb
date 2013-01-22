f83c22db5503eec865469149f74f323535e24e8b
Jonathan Bramble <phyjpb@leeds.ac.uk>
2013-01-22 22:35:00 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Owls::Application.config.secret_token = '60994142d9983f510655d8b26791b3e8767280f1b7af7fb251e5b61712fc89e83f2d0062df6b9c30c0fbfa9c612698ae8ed9ce73054a3c8ecc13c2c84cee5538'
